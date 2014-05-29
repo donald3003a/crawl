7eac287d60a2e934b369c9ebf8c8ebed057d1efa
ymhh <1018799688@qq.com>
2014-05-29 19:31:31 +0800

    邮件修改

--- src/main/java/com/xxx/crawl/service/impl/MailServiceImpl.java
+++ src/main/java/com/xxx/crawl/service/impl/MailServiceImpl.java
@@ -1,9 +1,8 @@
 package com.xxx.crawl.service.impl;
 
-import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailAttachment;
 import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
+import org.apache.commons.mail.MultiPartEmail;
 import org.springframework.stereotype.Service;
 
 import com.xxx.crawl.common.dto.MailDTO;
@@ -16,19 +15,20 @@ public class MailServiceImpl implements MailService {
 	public void sendMail(MailDTO mail) throws EmailException {
 		
 		EmailAttachment attachment = new EmailAttachment();
-		  attachment.setPath("mypictures/john.jpg");
+		  attachment.setPath("pom.xml");
 		  attachment.setDisposition(EmailAttachment.ATTACHMENT);
 		  attachment.setDescription("Picture of John");
-		  attachment.setName("John");
-		Email email = new SimpleEmail();
+		  attachment.setName("pom.xml");
+		  MultiPartEmail email = new MultiPartEmail(); 
 		email.setHostName("smtp.sina.com.cn");
 		email.setSmtpPort(25);
-		email.setAuthentication("lcgl_526", "lcgl123");
+		email.setAuthentication("lcgl_526@sina.com", "aaaaa888");
 		email.setSSLOnConnect(false);
 		email.setFrom("lcgl_526@sina.com");
 		email.setSubject("TestMail");
 		email.setMsg("This is a test mail ... :-)");
-		email.addTo("zhaowenzhen515@pingan.com");
+		email.addTo("liwei387@pingan.com");
+		email.attach(attachment);
 		email.send();
 	}
 
